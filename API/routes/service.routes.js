1b8c0f498af931e53be7edc23f9c6f5bfbd5a020
Enzo <120275012+Ko-rya@users.noreply.github.com>
2023-09-30 15:16:01 +0200

    models

---
+++ API/routes/service.routes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const Service = require("../models/Service");
+const Rating = require("../models/Rating");
