79b8abebd25a186d3a1f78b4654fa86c4e10cecc
Enzo <120275012+Ko-rya@users.noreply.github.com>
2023-10-03 21:08:07 +0200

    Models completed services routes

--- API/models/User.model.js
+++ API/models/User.model.js
@@ -1,11 +1,32 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: String,
-  username: String,
-  password: String,
-  gigs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Service" }],
-  phone_number: String,
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\S+@\S+\.\S+$/,
+  },
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  services: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Service",
+    },
+  ],
+  phone_number: {
+    type: String,
+    maxLength: 15,
+  },
   address: {},
   profilePicture: String,
 });
--- API/routes/service.routes.js
+++ API/routes/service.routes.js
@@ -1,5 +1,42 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
-const Service = require("../models/Service");
-const Rating = require("../models/Rating");
+const User = require("../models/User.model");
+const Service = require("../models/Service.model");
+const Rating = require("../models/Rating.model");
+
+router.post("/services", async (req, res) => {
+  try {
+    const { title, description, image, estimatepriceperday, ownerId } =
+      req.body;
+
+    const service = new Service({
+      title,
+      description,
+      image,
+      estimatepriceperday,
+      owner: ownerId,
+    });
+
+    await service.save();
+
+    res.status(201).json({ message: "Service created successfully", service });
+  } catch (error) {
+    console.error("Error creating service:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the service" });
+  }
+});
+
+router.get("/services", async (req, res) => {
+  try {
+    const services = await Service.find();
+    res.status(200).json({ services });
+  } catch (error) {
+    console.error("Error fetching services:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching services" });
+  }
+});
+module.exports = router;
