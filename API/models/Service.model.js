4757aaa8fe1e8bb6e17b3b0cc5f40fcfa9d8ca37
Nicolas Stevens <stvns.ncls@gmail.com>
2023-10-07 13:35:12 +0200

    test

--- API/models/Service.model.js
+++ API/models/Service.model.js
@@ -11,7 +11,7 @@ const serviceSchema = new Schema({
     maxLength: 2500,
   },
   image: String,
-  estimatepriceperday: {
+  estimatePricePerDay: {
     type: Number,
     required: true,
     min: 0,
